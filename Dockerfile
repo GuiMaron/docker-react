d8c9f1b9ae880789c14b4d1b87e6122eae6b74a5
Guilherme Maron <guimaron@gmail.com>
2022-11-08 16:36:58 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+
+#   BUILD
+FROM node:14-alpine AS builder
+
+WORKDIR /usr/app
+
+COPY ./package.json ./
+RUN npm install
+
+COPY ./ ./
+
+RUN npm run build
+
+
+
+#   PRODUCTION
+FROM nginx
+
+COPY --from=builder /usr/app/build/ /usr/share/nginx/html
+
+WORKDIR /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload, or not !!!
+          <br />
+          But Docker screwed it, nej?
+          <br />
+          Docker run makes no sense ...
+          <br />
+          But now  is composed ... =D
+          <br />
+          A little bit easier ... OH NOES (MOROES), OH YES !!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React (but Angular is better) !!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
